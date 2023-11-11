2b2a6f197d5d5086a00b8f68b81b3f5c03fa24f4
natali1503 <nfv1503@gmail.com>
2023-11-11 21:14:46 +0500

    add historyGame in reducer

--- src/randomizationPictures/randomizationPictures.js
+++ src/randomizationPictures/randomizationPictures.js
@@ -1,54 +1,54 @@
 function generateRandomNumber() {
-    return Math.trunc(Math.random() * 8);
+  return Math.trunc(Math.random() * 8);
 }
 
 function createRandomArray() {
-    let numbersCount = {};
-    let resultArray = [];
+  let numbersCount = {};
+  let resultArray = [];
 
-    for (let i = 0; i < 16; i++) {
-        let randomNumber = generateRandomNumber();
+  for (let i = 0; i < 16; i++) {
+    let randomNumber = generateRandomNumber();
 
-        while (numbersCount[randomNumber] >= 2) {
-            randomNumber = generateRandomNumber();
-        }
-
-        while (resultArray.filter(num => num === randomNumber).length >= 2) {
-            randomNumber = generateRandomNumber();
-        }
-
-        resultArray.push(randomNumber);
-        numbersCount[randomNumber] = (numbersCount[randomNumber] || 0) + 1;
+    while (numbersCount[randomNumber] >= 2) {
+      randomNumber = generateRandomNumber();
     }
 
-    return resultArray;
+    while (resultArray.filter((num) => num === randomNumber).length >= 2) {
+      randomNumber = generateRandomNumber();
+    }
+
+    resultArray.push(randomNumber);
+    numbersCount[randomNumber] = (numbersCount[randomNumber] || 0) + 1;
+  }
+
+  return resultArray;
 }
 
 function createTemplData(randomArray, imagePaths) {
-    const templData = [];
+  const templData = [];
 
-    for (let i = 0; i < randomArray.length; i++) {
-        const id = randomArray[i];
-        const position = i;
-        const img = imagePaths[id];
+  for (let i = 0; i < randomArray.length; i++) {
+    const id = randomArray[i];
+    const position = i;
+    const img = imagePaths[id];
 
-        templData.push({ id, position, img });
-    }
+    templData.push({ id, position, img });
+  }
 
-    return templData;
+  return templData;
 }
 
 let randomArray = createRandomArray();
 console.log(randomArray);
 let imagePaths = [
-    '../../public/images/003-flamingo.svg',
-    '../../public/images/008-hedgehog.svg',
-    '../../public/images/016-panda bear.svg',
-    '../../public/images/023-rabbit.svg',
-    '../../public/images/025-fox.svg',
-    '../../public/images/036-zebra.svg',
-    '../../public/images/038-elephant.svg',
-    '../../public/images/040-hippopotamus.sv',
+  "../../public/images/003-flamingo.svg",
+  "../../public/images/008-hedgehog.svg",
+  "../../public/images/016-panda%20bear.svg",
+  "../../public/images/023-rabbit.svg",
+  "../../public/images/025-fox.svg",
+  "../../public/images/036-zebra.svg",
+  "../../public/images/038-elephant.svg",
+  "../../public/images/040-hippopotamus.svg",
 ];
 
 let templData = createTemplData(randomArray, imagePaths);
