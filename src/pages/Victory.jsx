5076c3374e278ce6cea7646244ab41badfdb646e
natali1503 <nfv1503@gmail.com>
2023-11-12 18:41:05 +0500

    fix

--- src/pages/Victory.jsx
+++ src/pages/Victory.jsx
@@ -2,28 +2,29 @@ import { usePicture } from "../context/PictureContext";
 import Button from "../ui/Button";
 
 function Victory() {
-    const {dispatch, isShowHistory, time, moves} = usePicture();
-    return (
+  const { dispatch, isShowHistory, time, moves } = usePicture();
+  return (
+    <main>
+      <div className="wrapper">
         <div className="finish__wrapper">
-            <h2>Ура! У тебя получилось открыть все карточки</h2>
-            <p>время игры — {time}</p>
-            <p>количество ходов — {moves}</p>
-            <Button
-              onClick={() => dispatch({ type: "reset" })}
-              className="startBtn"
-            >
-              Сыграть ещё раз
-            </Button>
-            <Button
-              onClick={() => dispatch({ type: "showHistory" })}
-              className="startBtn"
-            >
-              {!isShowHistory ? "Показать историю партии" : "Следующий ход"}
-            </Button>
-          </div>
+          <h2>Ура! У тебя получилось открыть все карточки</h2>
+          <p>время игры — {time}</p>
+          <p>количество ходов — {moves}</p>
+          <Button
+            onClick={() => dispatch({ type: "reset" })}
+            className="startBtn"
+          >
+            Попробовать ещё
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "showHistory" })}
+            className="startBtn"
+          >
+            {!isShowHistory ? "Показать историю партии" : "Следующий ход"}
+          </Button>
         </div>
-      </main>
-    </body>
+      </div>
+    </main>
   );
 }
 
