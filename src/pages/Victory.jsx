e0f72273fc67c715b89441161995d84caa9dc6a2
Svetlana Govyazina <govyazina@gmail.com>
2023-11-12 12:39:51 +0200

    victory page

--- src/pages/Victory.jsx
+++ src/pages/Victory.jsx
@@ -1,13 +1,16 @@
 import { usePicture } from "../context/PictureContext";
 import Reset from "../ui/Reset";
+import Button from "../ui/Button.jsx";
 
 function Victory() {
   const { dispatch } = usePicture();
   return (
         <div className="finish__wrapper">
             <h2>Ура! Ты открыл все карточки!</h2>
-            <Reset onClick={() => dispatch({ type: "reset" })} />
-            <button className="startBtn">Попробовать ещё</button>
+            <Button
+                onClick={() => dispatch({ type: "reset" })}
+                children={'Попробовать ещё'}
+            />
         </div>
 
   );
