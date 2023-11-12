1ca7e217895a94561ac65ab56fbd8298f102fe98
Svetlana Govyazina <govyazina@gmail.com>
2023-11-12 15:20:03 +0200

    fix text for start and finish pages

--- src/pages/StartGame.jsx
+++ src/pages/StartGame.jsx
@@ -1,15 +1,21 @@
-import { usePicture } from "../context/PictureContext";
+import {usePicture} from "../context/PictureContext";
 import Button from "../ui/Button";
 
 function StartGame() {
-  const { dispatch } = usePicture();
-  return (
-    <Button
-      className="startBtn"
-      onClick={() => dispatch({ type: "startGame" })}
-      children={"Начать игру"}
-    />
-  );
+    const {dispatch} = usePicture();
+    return (
+        <>
+            <Button
+                className="startBtn"
+                onClick={() => dispatch({type: "startGame"})}
+                children={"Начать игру"}
+            />
+            <h2>Открывай по очереди карточки,
+                запоминай какие зверюшки там появляются
+                и найди всех повторяющихся
+            </h2>
+        </>
+    );
 }
 
 export default StartGame;
